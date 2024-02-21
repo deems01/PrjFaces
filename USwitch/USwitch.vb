d1d8f6728e7a81b07718ce27a35ceb2eb2d9a26f
Uku-Erik Hindrikson <ukuerik.hindrikson@gmail.com>
2024-02-21 15:54:59 +0200

    wip

--- USwitch/USwitch.vb
+++ USwitch/USwitch.vb
@@ -1,5 +1,5 @@
 ﻿Public Class USwitch
-    Dim state As Integer = 0
+    Dim switch As Integer = 0
     Public Event switchState(state As Integer)
     Private Sub selectedSwitch(ByVal intstate As Integer)
         If Not IsNothing(pbSwitch.Image) Then
@@ -10,10 +10,10 @@
         pbSwitch.Image = imgSwitch.Images(intstate)
     End Sub
 
-    Public Sub setSwitch(ByVal state As String)
-        If state = "ON" Then
+    Public Sub setSwitch(ByVal switch As String)
+        If switch = "ON" Then
             Me.selectedSwitch(0)
-        ElseIf state = "OFF" Then
+        ElseIf switch = "OFF" Then
             Me.selectedSwitch(1)
         Else
             Me.selectedSwitch(1)
@@ -21,13 +21,13 @@
     End Sub
 
     Private Sub pbSwitch_Click(sender As Object, e As EventArgs) Handles pbSwitch.Click
-        If state = 0 Then
+        If switch = 0 Then
             setSwitch("ON")
-            state = 1
+            switch = 1
         Else
             setSwitch("OFF")
-            state = 0
+            switch = 0
         End If
-        RaiseEvent switchState(state)
+        RaiseEvent switchState(switch)
     End Sub
 End Class
